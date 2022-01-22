7ec553fce010614aa57837f30f331d59528bafe2
dreamer20 <gv4alex@gmail.com>
2022-01-22 23:56:37 +0300

    Add filter, rotate and resize functions

--- imgtools/api.py
+++ imgtools/api.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, request, jsonify
+from PIL import Image, ImageFilter,  ImageEnhance, ImageChops, ImageOps
+from io import BytesIO
+from flask import Blueprint, request, jsonify, send_file
 
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'gif', 'png'}
 bp = Blueprint('api', __name__, url_prefix='/api')
@@ -25,6 +27,7 @@ def withFileCheck(f):
             return f(*args, **kwargs)
         else:
             return jsonify({'error':'Расширение не поддерживается'}), 400
+    wrapper.__name__ = f.__name__
     return wrapper
 
 
@@ -37,3 +40,79 @@ def reflect():
     # if transformation isn't successful send error
     # save image
     # send image to client
+
+
+@bp.route('/resize', methods=['POST'])
+@withFileCheck
+def resize():
+    file = request.files['image']
+    width = int(request.form['width'])
+    height = int(request.form['height'])
+    bytes_io = BytesIO()
+
+    with Image.open(file) as img:
+        resultImg = img.resize((width, height))
+        resultImg.save(bytes_io, format=img.format)
+    bytes_io.seek(0)
+
+    return send_file(bytes_io, mimetype=f'image/{img.format.lower()}')
+
+
+@bp.route('/rotate', methods=['POST'])
+@withFileCheck
+def rotate():
+    file = request.files['image']
+    degree = int(request.form['degree'])
+    transpose_operations = {
+        90: Image.ROTATE_90,
+        180: Image.ROTATE_180,
+        270: Image.ROTATE_270
+    }
+    bytes_io = BytesIO()
+
+    with Image.open(file) as img:
+        if (degree in transpose_operations):
+            resultImg = img.transpose(transpose_operations[degree])
+        else:
+            resultImg = img.rotate(degree)
+        resultImg.save(bytes_io, format=img.format)
+    bytes_io.seek(0)
+
+    return send_file(bytes_io, mimetype=f'image/{img.format.lower()}')
+
+
+@bp.route('/filter', methods=['POST'])
+@withFileCheck
+def applyFilter():
+    file = request.files['image']
+    filterName = request.form['filterName']
+    imageFilters = (
+        'BLUR',
+        'DETAIL'
+    )
+    bytes_io = BytesIO()
+
+    with Image.open(file) as img:
+        if (filterName in imageFilters):
+            resultImg = img.filter(getattr(ImageFilter, filterName))
+        else:
+            return jsonify({'error': 'Операция не выполнима.'}), 400
+        resultImg.save(bytes_io, format=img.format)
+    bytes_io.seek(0)
+
+    return send_file(bytes_io, mimetype=f'image/{img.format.lower()}')
+
+
+@bp.route('/test', methods=['POST'])
+@withFileCheck
+def test():
+    file = request.files['image']
+    file2 = request.files['image2']
+    bytes_io = BytesIO()
+
+    with Image.open(file) as img:
+        resultImg = ImageOps.invert(img)
+        resultImg.save(bytes_io, format=img.format)
+    bytes_io.seek(0)
+
+    return send_file(bytes_io, mimetype=f'image/{img.format.lower()}')
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from imgtools import create_app
 
 
@@ -12,3 +13,14 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture
+def image_sample():
+    img_path = os.path.join(os.path.dirname(__file__), 'test_image3.jpg')
+    return open(img_path, 'rb')
+
+@pytest.fixture
+def image_sample2():
+    img_path = os.path.join(os.path.dirname(__file__), 'test_image4.jpg')
+    return open(img_path, 'rb')
